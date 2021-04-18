cdaca181ed4d171d1a468a540777ff9f6432593c
MilosSimic994 <60389147+MilosSimic994@users.noreply.github.com>
2021-04-18 13:24:06 +0200

    player choice

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 const modal = document.getElementById("modal");
 const rulesBtn = document.getElementById("rules");
 const closeBtn = document.getElementById("closeBtn");
+const startOption = document.querySelector(".start__option");
+
+// const paper = doucment.getElementById("paper");
+// const scissors = doucment.getElementById("scissors");
+// const rock = doucment.getElementById("rock");
+
+const choices = startOption.querySelectorAll("img");
+console.log(choices);
+
+let player;
+
+//playerr choice
+const playerChoice = (e) => {
+  player = e.target.getAttribute("id").toUpperCase();
+  console.log(player);
+};
+
+// compiuter choice
+
+const compiuterChoice = () => {};
 
 //show rules
 const openRules = () => {
@@ -17,3 +37,7 @@ const closeRules = (e) => {
 rulesBtn.addEventListener("click", openRules);
 closeBtn.addEventListener("click", closeRules);
 modal.addEventListener("click", closeRules);
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", playerChoice);
+});
